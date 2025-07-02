33b29c59bc7137e215da4dca483bfd7715e30965
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-02 16:35:13 -0600

    modularised the connect function and exported it to play.js

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',// IP address here,
+    port: 50541,// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('connection succeful')
+  });
+
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+
+  return conn;
+};
+
+module.exports = connect;
